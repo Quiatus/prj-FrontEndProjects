ea2f0a7f396d7c66a820ce402f166f1b4b8b2fc8
Quiatus <Quiatus@gmail.com>
2024-02-23 12:06:25 +0100

    complete project 08

---
+++ projects/08_rating_card/script/main.js
@@ -0,0 +1,27 @@
+const btnCircle = document.querySelectorAll('.btn-circle')
+const btnSubmit = document.querySelector('#btn-submit')
+const ratingDiv = document.querySelector('.rating')
+const resultDiv = document.querySelector('.result')
+const resultText = document.querySelector('#result-text');
+
+let rating = null
+
+const switchToResult = () => {
+    ratingDiv.style.display = 'none'
+    resultDiv.style.display = 'flex'
+    resultText.textContent = `You selected ${rating} out of 5`
+}
+
+btnCircle.forEach(btn => {btn.addEventListener('click', () => {
+    btnCircle.forEach(btn => {btn.classList.remove('selected')})
+    rating = btn.textContent;
+    btn.classList.add('selected')
+})})
+
+btnSubmit.addEventListener('click', () => {
+    if (rating) {
+        switchToResult()
+    } else {
+        alert('Please choose rating')
+    }
+})
