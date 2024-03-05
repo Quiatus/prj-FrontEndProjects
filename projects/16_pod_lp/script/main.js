fd53c96de0a19a38770b59683831768650540fc7
Quiatus <Quiatus@gmail.com>
2024-03-05 15:22:16 +0100

    complete project 16

---
+++ projects/16_pod_lp/script/main.js
@@ -0,0 +1,28 @@
+const emailInput = document.getElementById('input-email');
+const btnSubmit = document.getElementById('btn-submit');
+const statusText = document.getElementById('text-status');
+
+const validateEmail = (email) => {
+    return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  };
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    statusText.classList.remove('text-error', 'text-green')
+
+    if (!emailInput.value) {
+        statusText.classList.add('text-error')
+        statusText.textContent = 'Please enter email address!' 
+        return  
+    }
+       
+    if (validateEmail(emailInput.value)) {
+        statusText.classList.add('text-green')
+        statusText.textContent = 'Access requested!'
+        emailInput.value = '';
+    } else {
+        statusText.classList.add('text-error')
+        statusText.textContent = 'Invalid email format!' 
+    }
+})
