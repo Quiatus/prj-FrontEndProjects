264706434d9dbb2e9070ecd8b4289ebf23eec10f
Quiatus <Quiatus@gmail.com>
2024-03-14 14:23:55 +0100

    complete project 21

--- projects/21_apparel_page/script/main.js
+++ projects/21_apparel_page/script/main.js
@@ -1,28 +1,41 @@
-// const emailInput = document.getElementById('input-email');
-// const btnSubmit = document.getElementById('btn-submit');
-// const statusText = document.getElementById('text-status');
+const emailInput = document.getElementById('email');
+const btnSubmit = document.getElementById('submit-button');
+const statusText = document.getElementById('error-text');
+const imgErr = document.getElementById('error-img');
+const form = document.querySelector('.form');
 
-// const validateEmail = (email) => {
-//     return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-//   };
+const validateEmail = (email) => {
+    return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  };
 
-// btnSubmit.addEventListener('click', (e) => {
-//     e.preventDefault();
+const displayMessage = (msg = 'correct') => {
+    form.classList.add('error-box')
+    imgErr.style.visibility = 'visible'
 
-//     statusText.classList.remove('text-error', 'text-green')
+    if (msg === 'empty') {
+        statusText.textContent = 'Please enter email address!'
+    } else if (msg === 'invalid') {
+        statusText.textContent = 'Invalid email format!'
+    } else {
+        form.classList.remove('error-box')
+        statusText.textContent = 'Thank you!'
+        imgErr.style.visibility = 'hidden'
+        emailInput.value = ''; 
+    }
+}
 
-//     if (!emailInput.value) {
-//         statusText.classList.add('text-error')
-//         statusText.textContent = 'Please enter email address!' 
-//         return  
-//     }
-       
-//     if (validateEmail(emailInput.value)) {
-//         statusText.classList.add('text-green')
-//         statusText.textContent = 'Access requested!'
-//         emailInput.value = '';
-//     } else {
-//         statusText.classList.add('text-error')
-//         statusText.textContent = 'Invalid email format!' 
-//     }
-// })
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(emailInput.value)) {
+        !emailInput.value ? displayMessage('empty') : displayMessage('invalid')
+    } else {
+        displayMessage(); 
+    }
+})
+
+emailInput.addEventListener('keypress', () => {
+    form.classList.remove('error-box')
+    statusText.textContent = ''
+    imgErr.style.visibility = 'hidden'
+})
